189a9141d33d3f5e5574fc35bffe7fd8d73367a7
Rohan <rohanbarkade8085@gmail.com>
2026-07-05 00:01:15 +0530

    Added CsharpDemo projects

---
+++ CsharpDemo/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Xml.Linq;
+
+namespace ConsoleApp1
+{
+    interface IPrinter
+    {
+        void Print();
+    }
+    interface IScanner
+    {
+        void Scan();
+    }
+    class Machine : IPrinter, IScanner
+    {
+        public void Print()
+        {
+            Console.WriteLine("Printing...");
+        }
+        public void Scan()
+        {
+            Console.WriteLine("Scanning...");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Machine m = new Machine();
+            m.Print();
+            m.Scan();
+        }
+    }
+}
---
+++ CsharpDemo/ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using MathLibrary;
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main()
+        {
+            Calculator c = new Calculator();
+            Console.WriteLine(c.Add(10, 20));
+            Console.WriteLine(c.Multiply(10, 20));
+        }
+    }
+}

bcb245eb1492a1b078a541250ed42baf94fd2bf8
Rohan <rohanbarkade8085@gmail.com>
2026-07-12 23:45:39 +0530

    Add CsharpDemo example

---
+++ CsharpDemo/Generics/Generics/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+namespace Generics
+{
+    //class Demo<T>
+    //{
+    //    public T Value;
+    //    public void Show()
+    //    {
+    //        Console.WriteLine(Value);
+    //    }
+    //}
+    class Program
+    {
+        static void Display<T>(T value)
+        {
+            Console.WriteLine(value);
+        }
+
+        static void Main()
+        {
+            //Demo<int> d1 = new Demo<int>();
+            //d1.Value = 100;
+            //d1.Show();
+
+            //Demo<string> d2 = new Demo<string>();
+            //d2.Value = "hello";
+            //d2.Show();
+            Display(100);
+            Display("CDAC");
+            Display(5.5);
+
+        }
+    }
+}
